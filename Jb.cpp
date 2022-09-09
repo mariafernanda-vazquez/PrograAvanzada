fd7968a779db1f615394105bf19784bcc11da7cd
mariafernanda-vazquez <83107598+mariafernanda-vazquez@users.noreply.github.com>
2022-09-09 08:05:15 -0500

    Update Jb.cpp

--- Jb.cpp
+++ Jb.cpp
@@ -4,43 +4,51 @@
 #include <vector>
 using namespace std;
 
-
 int main(int argc, char const *argv[])
 {
-    int primer=2, segundo=2, tercer=3, cuarto=4;
-    int total=pow(primer,segundo);
-    bitset<255>B(pow(2,4)-1);
-    cout<<B; 
+    int primer=10, segundo=5, tercer=1, cuarto=3, quinto=5, sexto=6, septimo=1024;
+    int total=pow(2,primer);
+    bitset<1040>B;
+    B.set();
     int count=0;
-    for (int i = 0; i < 4; i+=2)
+    for (int i = 0; i < total; i++)
     {
-        B[4-3]=false;
-        B[4-4]=false;
+        B[total-tercer]=false;
+        B[total-cuarto]=false;
+        B[total-quinto]=false;
+        B[total-sexto]=false;
+        B[total-septimo]=false;
     }
-    cout<<B<<endl;
-     vector <int> vector;
-        for (int i = 0; i < 4; i+=2)
+    cout<<B;
+    int indice;
+    total=total+2;
+    while(primer--){
+        total=total-2;
+        indice=0;
+        cout<<"total"<<total<<endl;
+        for (int i = 0; i < total; i+=2)
         {
         cout<<B[i]<<B[i+1]<<endl;
         if(B[i] ^ B[i+1]){
             count++;
-            cout<<"1";
-            vector.push_back(true);
+            B[indice++]=true;
         }
         else if(B[i] | B[i+1]){
-            cout<<"2";
-            vector.push_back(true);
+            B[indice++]=true;
         }
         else{
-            cout<<"3";
-            vector.push_back(false);
+            B[indice++]=false;
         }
+        cout<<"contador"<<count<<endl;
+        cout<<B[0];
+        cout<<B[1];
+        cout<<B[2];
+        cout<<B[3];
      }
-     cout<<B<<endl;
+    }
 
-     for (int i = 0; i < vector.size(); i++)
-     {
-        cout<<vector[i];
-     }
+     cout<<count<<endl;
+     cout<<indice<<endl;
+     cout<<B<<endl;
     return 0;
 }
